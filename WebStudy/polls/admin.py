a9753de98d6ceabe78dd9c752d639d38768a0823
huangbaoyu <huang_baoyu@163.com>
2017-05-17 18:20:29 +0800

    视图和模型~

--- WebStudy/polls/admin.py
+++ WebStudy/polls/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from .models import Question,Choice
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date')
+    # 添加的过滤器
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
 admin.site.register(Question,QuestionAdmin)
--- WebStudy/polls/models.py
+++ WebStudy/polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('aaaa')
+    pub_date = models.DateTimeField('时间')
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
